091ca983d6e42df07af9dfcc0a6a4795fa497f2e
hmlab book pro <saki.tommy69@gmail.com>
2016-02-24 17:17:31 +0900

    変更しました

---
+++ thchmonster/LobbyViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LobbyViewController.swift
+//  thchmonster
+//
+//  Created by hmlab book pro on 2016/02/24.
+//  Copyright © 2016年 hmlab book pro. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class LobbyViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var stamina: Float = 0
+    var staminaTimer: NSTimer!
+    var util: TechDraUtility!
+    var player: Player!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaBar: UIProgressView!
+    @IBOutlet var levelLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        player =  Player()
+        
+        staminaBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        var level: Int = userDefaults.integerForKey("level")
+        
+        nameLabel.text = player.name
+        levelLabel.text =  String(format: "Lv.%d", level + 1)
+        stamina = 100
+        
+        util = TechDraUtility()
+        cureStamina()
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        util.playBGM("lobby")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        util.stopBGM()
+    }
+    
+    //MARK: Cure
+    func cureStamina(){
+        staminaTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateStaminaValue", userInfo: nil, repeats: true)
+        staminaTimer.fire()
+        
+    }
+    
+    func updateStaminaValue(){
+        if stamina <= 100{
+            stamina = stamina + 1
+            staminaBar.progress = stamina / 100
+        }
+    }
+    
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- thchmonster/ViewController.swift
+++ thchmonster/ViewController.swift
@@ -9,17 +9,142 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer: NSTimer!
+    var enemyTimer: NSTimer!
+    
+    var enemy: Enemy!
+    var player: Player!
+    
+    //make thchDraUtility
+    
+    let util: TechDraUtility = TechDraUtility()
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var attackButton: UIButton!
+    
+    @IBOutlet var enemyImageView:UIImageView!
+    @IBOutlet var playerImageView:UIImageView!
+    
+    @IBOutlet var enemyHPBar: UIProgressView!
+    @IBOutlet var playerHPBar: UIProgressView!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var playerNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        enemyHPBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        playerHPBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        initStatus()
+        
+        enemyTimer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(enemy.speed), target: self, selector: "enemyAttack", userInfo: nil, repeats: true)
+        enemyTimer.fire()
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initStatus(){
+        
+        enemy = Enemy()
+        player = Player()
+        
+        enemyNameLabel.text = enemy.name
+        playerNameLabel.text = player.name
+        
+        enemyImageView.image = enemy.image
+        playerImageView.image = player.image
+        
+        enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+        playerHPBar.progress = player.currentHP / player.maxHP
+        
+        cureHP()
+        
     }
-
-
+    
+    override func viewDidAppear(animated: Bool) {
+        util.playBGM("BGW_battle001")
+    }
+    
+    @IBAction func playerAttack(){
+        TechDraUtility.damageAnimation(enemyImageView)
+        util.playBGM("SE_attack")
+        
+        enemy.currentHP = enemy.currentHP - player.attackPoint
+        enemyHPBar.setProgress(enemy.currentHP / enemy.maxHP,  animated: true)
+        
+        if enemy.currentHP <= 0.0{
+            finishBattle(playerImageView, winPlayer: false)
+            
+        }
+    }
+    
+    func enemyAttack(){
+        TechDraUtility.damageAnimation(playerImageView)
+        util.playBGM("SE_attack")
+        
+        player.currentHP = player.currentHP - enemy.attackPoint
+        playerHPBar.setProgress(player.currentHP / player.maxHP,  animated: true)
+        
+        if enemy.currentHP <= 0.0{
+            finishBattle(playerImageView, winPlayer: false)
+        }
+    }
+    
+    func finishBattle(vanishImageView: UIImageView, winPlayer: Bool){
+        TechDraUtility.vanishAnimation(vanishImageView)
+        util.stopBGM()
+        timer.invalidate()
+        enemyTimer.invalidate()
+        
+        var finishedMessage: String!
+        
+        if attackButton.hidden != true{
+            attackButton.hidden = true
+        }
+        
+        if winPlayer == true{
+            util.playSE("SE_fanfare")
+            finishedMessage = "プレイヤーの勝利！"
+            
+        }else{
+            util.playSE("SE_gameover")
+            finishedMessage = "プレイヤーの敗北！"
+        }
+        
+        var alert = UIAlertController(title: "バトル終了", message: finishedMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(
+            UIAlertAction(
+                title: "OK", style: .Default, handler: { action in
+                    self.dismissViewControllerAnimated(true, completion: nil)
+            }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    //mark: cure
+    func cureHP(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateHPValue", userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+    func updateHPValue(){
+        if enemy.currentHP < enemy.maxHP{
+            enemy.currentHP = enemy.currentHP + enemy.defencePoint
+            enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+            
+        }
+        
+        if player.currentHP < player.maxHP{
+            player.currentHP = player.currentHP + player.defencePoint
+            playerHPBar.progress = player.currentHP / player.maxHP
+        }
+        
+    }
+    
+    
 }
 
